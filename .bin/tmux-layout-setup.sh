a62155b9e8c2462f158c3e1eb09d35fb9c1911df
mocaffy <mocaffy@gmail.com>
2023-02-04 20:11:05 +0900

    .bin ディレクトリを追加

---
+++ .bin/tmux-layout-setup.sh
@@ -0,0 +1,52 @@
+#!/bin/zsh
+
+# セッションの名前
+SESSION_NAME=config
+
+# ワークスペースの数
+WORKSPACE_COUNT=4
+
+# ワークスペース(タブ)の名前
+WORKSPACE_NAME=(
+  neovim
+  yabai
+  alacritty
+  tmux
+)
+
+# ワークスペースのパス
+WORKSPACE_PATH=(
+  ~/.config/nvim/
+  ~/.config/yabai/
+  ~/.config/alacritty/
+  ~/.config/tmux/
+)
+
+# 同じレイアウトで複数のワークスペースを作成
+for ((I=1; I<=$WORKSPACE_COUNT; I++)); do
+  if [ $I = 1 ]; then
+    # 1つ目のワークスペースはセッション作成と同時に作成
+    tmux new-session -d -A -s "$SESSION_NAME" -c $WORKSPACE_PATH[$I] -x 64 -y 64
+  else
+    # 以降のワークスペースはウィンドウを作成
+    tmux new-window -c $WORKSPACE_PATH[$I]
+  fi
+  # neovim を起動
+  tmux send-keys 'nvim' Enter
+  # ウィンドウの名前を変更
+  tmux rename-window "$WORKSPACE_NAME[$I]"
+  # 縦 25% の位置で分割
+  tmux split-window -v -p 25 -c "#{pane_current_path}"
+  # 横 50% の位置で分割
+  tmux split-window -h -p 50 -c "#{pane_current_path}"
+  # メインのペインにフォーカスしてズーム
+  tmux select-pane -t 0
+  tmux resize-pane -Z
+done
+
+
+# 最初のウィンドウにフォーカスする
+tmux select-window -t 0
+
+# 現在のターミナルをセッションにアタッチする
+tmux attach -t $SESSION_NAME
