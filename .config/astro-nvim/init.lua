82e12190eedeb294957d1170eba1584ecef796c7
mocaffy <mocaffy@gmail.com>
2023-02-04 23:56:18 +0900

    nvim ayu の override を dark に限定

--- .config/astro-nvim/init.lua
+++ .config/astro-nvim/init.lua
@@ -265,16 +265,20 @@ local config = {
           colors.generate(true) -- Pass `true` to enable mirage
           require("ayu").setup {
             overrides = function()
-              return {
-                VertSplit = { fg = "#171d26", bg = "#171d26" },
-                StatusLine = { fg = colors.fg, bg = "#171d26" },
-                StatusLineNC = { fg = colors.fg_idle, bg = "#171d26" },
-                Normal = { fg = colors.fg, bg = "#1f2733" },
-                NormalNC = { fg = colors.fg, bg = "#1b222d" },
-                SignColumn = {},
-                CursorLine = { bg = "#1F252F" },
-                MsgArea = { fg = "#e1c98e", bg = "#171d26" },
-              }
+              if vim.o.background == 'dark' then
+                return {
+                  VertSplit = { fg = "#171d26", bg = "#171d26" },
+                  StatusLine = { fg = colors.fg, bg = "#171d26" },
+                  StatusLineNC = { fg = colors.fg_idle, bg = "#171d26" },
+                  Normal = { fg = colors.fg, bg = "#1f2733" },
+                  NormalNC = { fg = colors.fg, bg = "#1b222d" },
+                  SignColumn = {},
+                  CursorLine = { bg = "#1F252F" },
+                  MsgArea = { fg = "#e1c98e", bg = "#171d26" },
+                }
+              else
+                return {}
+              end
             end,
           }
         end,
