3d62f33ae511b01f314bfaac75c0ed33201bf16c
mocaffy <mocaffy@gmail.com>
2023-02-06 03:37:43 +0900

    nvim Lsp と TS の自動インストールを設定

--- .config/astro-nvim/init.lua
+++ .config/astro-nvim/init.lua
@@ -393,11 +393,11 @@ local config = {
       return config -- return final config table
     end,
     treesitter = { -- overrides `require("treesitter").setup(...)`
-      -- ensure_installed = { "lua" },
+      ensure_installed = { "lua", "vim", "typescript", "javascript", "html", "pug", "css" },
     },
     -- use mason-lspconfig to configure LSP installations
     ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
-      -- ensure_installed = { "sumneko_lua" },
+      ensure_installed = { "sumneko_lua", "vimls", "angularls", "tsserver" },
     },
     -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
     ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
