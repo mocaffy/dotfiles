8f058bc62c471e1c7e25f1c3f35457e128f570da
mocaffy <mocaffy@gmail.com>
2023-12-18 01:56:04 +0900

    update

--- docker/editor.sh
+++ docker/editor.sh
@@ -1,4 +1,12 @@
-docker run --rm -it \
+LOCAL_UID=$(id -u)
+LOCAL_GID=$(id -g)
+
+docker run \
+  --rm -it \
+  -e LOCAL_UID=$LOCAL_UID \
+  -e LOCAL_GID=$LOCAL_GID \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   -v ~/.gitconfig:/etc/gitconfig \
   -v ~/development/:/root/development/ \
-  mocaffy/dotfiles:base
+  editor:base \
+  "/home/user/dotfiles/.bin/tmux-layout-setup.sh"
--- docker/editor/entrypoint.sh
+++ docker/editor/entrypoint.sh
@@ -6,6 +6,7 @@ GROUP_ID=${LOCAL_GID:-9001}
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
 usermod -u $USER_ID -o -m -d /home/user user
 groupmod -g $GROUP_ID user
+
 export HOME=/home/user
 echo "alias angular='docker compose -f /home/mocaffy/docker-compose.yml exec angular zsh -c ng v'" >> /home/user/.zshrc
-exec /usr/sbin/gosu user "$@"
+exec su-exec user "$@"
