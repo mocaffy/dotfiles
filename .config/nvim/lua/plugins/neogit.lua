ab6db7b1a937548e5b79fc0d7edeaa4a5aa4a666
mocaffy <mocaffy@gmail.com>
2024-12-23 01:09:01 +0900

    neogit config

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,35 +1,44 @@
-  -- 行単位でコミットできるプラグイン
+-- 行単位でコミットできるプラグイン
 return {
   "mocaffy/neogit",
-  dependencies = 'nvim-lua/plenary.nvim',
+  -- "NeogitOrg/neogit",
+  dependencies = "nvim-lua/plenary.nvim",
   -- enabled = false,
   config = function()
-    require("neogit").setup({
+    require("neogit").setup {
       -- Hides the hints at the top of the status buffer
-      disable_hint = true,
+      disable_hint = false,
       -- Disables changing the buffer highlights based on where the cursor is.
       disable_context_highlighting = false,
       -- Disables signs for sections/items/hunks
       disable_signs = false,
-      -- Do not ask to confirm the commit - just do it when the buffer is closed.
-      disable_commit_confirmation = false,
-      -- Uses `vim.notify` instead of the built-in notification system.
-      disable_builtin_notifications = false,
       -- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to
       -- insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in
       -- normal mode.
-      disable_insert_on_commit = true,
+      disable_insert_on_commit = "auto",
       -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
       -- events.
       filewatcher = {
         interval = 1000,
-        enabled = false,
+        enabled = true,
+      },
+      -- "ascii"   is the graph the git CLI generates
+      -- "unicode" is the graph like https://github.com/rbong/vim-flog
+      -- "kitty"   is the graph like https://github.com/isakbm/gitgraph.nvim - use https://github.com/rbong/flog-symbols if you don't use Kitty
+      graph_style = "ascii",
+      -- Show relative date by default. When set, use `strftime` to display dates
+      commit_date_format = nil,
+      log_date_format = nil,
+      -- Used to generate URL's for branch popup action "pull request".
+      git_services = {
+        ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+        ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+        ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+        ["azure.com"] = "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
       },
       -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
       -- sorter instead. By default, this function returns `nil`.
-      telescope_sorter = function()
-        return require("telescope").extensions.fzf.native_fzf_sorter()
-      end,
+      telescope_sorter = function() return require("telescope").extensions.fzf.native_fzf_sorter() end,
       -- Persist the values of switches/options within and across sessions
       remember_settings = true,
       -- Scope persisted settings on a per-project basis
@@ -42,6 +51,14 @@ return {
         "NeogitCommitPopup--allow-empty",
         "NeogitRevertPopup--no-edit",
       },
+      -- Configure highlight group features
+      highlight = {
+        italic = true,
+        bold = true,
+        underline = true,
+      },
+      -- Set to false if you want to be responsible for creating _ALL_ keymappings
+      use_default_keymaps = true,
       -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
       -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
       auto_refresh = true,
@@ -50,42 +67,93 @@ return {
       -- Flag description: https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---sortltkeygt
       -- Sorting keys: https://git-scm.com/docs/git-for-each-ref#_options
       sort_branches = "-committerdate",
+      -- Default for new branch name prompts
+      initial_branch_name = "",
       -- Change the default way of opening neogit
       kind = "tab",
+      -- Disable line numbers
+      disable_line_numbers = true,
+      -- Disable relative line numbers
+      disable_relative_line_numbers = true,
       -- The time after which an output console is shown for slow running commands
       console_timeout = 2000,
       -- Automatically show console if a command takes more than console_timeout milliseconds
       auto_show_console = true,
+      -- Automatically close the console if the process exits with a 0 (success) status
+      auto_close_console = true,
+      notification_icon = "󰊢",
       status = {
+        show_head_commit_hash = true,
         recent_commit_count = 10,
+        HEAD_padding = 10,
+        HEAD_folded = false,
+        mode_padding = 3,
+        mode_text = {
+          M = "modified",
+          N = "new file",
+          A = "added",
+          D = "deleted",
+          C = "copied",
+          U = "updated",
+          R = "renamed",
+          DD = "unmerged",
+          AU = "unmerged",
+          UD = "unmerged",
+          UA = "unmerged",
+          DU = "unmerged",
+          AA = "unmerged",
+          UU = "unmerged",
+          ["?"] = "",
+        },
       },
       commit_editor = {
-        kind = "split",
+        kind = "tab",
+        show_staged_diff = true,
+        -- Accepted values:
+        -- "split" to show the staged diff below the commit editor
+        -- "vsplit" to show it to the right
+        -- "split_above" Like :top split
+        -- "vsplit_left" like :vsplit, but open to the left
+        -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
+        staged_diff_split_kind = "split",
+        spell_check = true,
       },
       commit_select_view = {
         kind = "tab",
       },
       commit_view = {
         kind = "vsplit",
-        verify_commit = os.execute("which gpg") == 0, -- Can be set to true or false, otherwise we try to find the binary
+        verify_commit = vim.fn.executable "gpg" == 1, -- Can be set to true or false, otherwise we try to find the binary
       },
       log_view = {
         kind = "tab",
       },
       rebase_editor = {
-        kind = "split",
+        kind = "auto",
       },
       reflog_view = {
         kind = "tab",
       },
       merge_editor = {
-        kind = "split",
+        kind = "auto",
+      },
+      description_editor = {
+        kind = "auto",
+      },
+      tag_editor = {
+        kind = "auto",
       },
       preview_buffer = {
-        kind = "split",
+        kind = "floating_console",
       },
       popup = {
         kind = "split",
+      },
+      stash = {
+        kind = "tab",
+      },
+      refs_view = {
+        kind = "tab",
       },
       signs = {
         -- { CLOSED, OPENED }
@@ -103,11 +171,16 @@ return {
         --
         -- Requires you to have `sindrets/diffview.nvim` installed.
         diffview = nil,
-    
+
         -- If enabled, uses fzf-lua for menu selection. If the telescope integration
         -- is also selected then telescope is used instead
         -- Requires you to have `ibhagwan/fzf-lua` installed.
         fzf_lua = nil,
+
+        -- If enabled, uses mini.pick for menu selection. If the telescope integration
+        -- is also selected then telescope is used instead
+        -- Requires you to have `echasnovski/mini.pick` installed.
+        mini_pick = nil,
       },
       sections = {
         -- Reverting/Cherry Picking
@@ -157,6 +230,40 @@ return {
         },
       },
       mappings = {
+        commit_editor = {
+          ["q"] = "Close",
+          ["<c-c><c-c>"] = "Submit",
+          ["<c-c><c-k>"] = "Abort",
+          ["<m-p>"] = "PrevMessage",
+          ["<m-n>"] = "NextMessage",
+          ["<m-r>"] = "ResetMessage",
+        },
+        commit_editor_I = {
+          ["<c-c><c-c>"] = "Submit",
+          ["<c-c><c-k>"] = "Abort",
+        },
+        rebase_editor = {
+          ["p"] = "Pick",
+          ["r"] = "Reword",
+          ["e"] = "Edit",
+          ["s"] = "Squash",
+          ["f"] = "Fixup",
+          ["x"] = "Execute",
+          ["d"] = "Drop",
+          ["b"] = "Break",
+          ["q"] = "Close",
+          ["<cr>"] = "OpenCommit",
+          ["gk"] = "MoveUp",
+          ["gj"] = "MoveDown",
+          ["<c-c><c-c>"] = "Submit",
+          ["<c-c><c-k>"] = "Abort",
+          ["[c"] = "OpenOrScrollUp",
+          ["]c"] = "OpenOrScrollDown",
+        },
+        rebase_editor_I = {
+          ["<c-c><c-c>"] = "Submit",
+          ["<c-c><c-k>"] = "Abort",
+        },
         finder = {
           ["<cr>"] = "Select",
           ["<c-c>"] = "Close",
@@ -165,54 +272,78 @@ return {
           ["<c-p>"] = "Previous",
           ["<down>"] = "Next",
           ["<up>"] = "Previous",
-          ["<tab>"] = "MultiselectToggleNext",
-          ["<s-tab>"] = "MultiselectTogglePrevious",
+          ["<tab>"] = "InsertCompletion",
+          ["<space>"] = "MultiselectToggleNext",
+          ["<s-space>"] = "MultiselectTogglePrevious",
           ["<c-j>"] = "NOP",
+          ["<ScrollWheelDown>"] = "ScrollWheelDown",
+          ["<ScrollWheelUp>"] = "ScrollWheelUp",
+          ["<ScrollWheelLeft>"] = "NOP",
+          ["<ScrollWheelRight>"] = "NOP",
+          ["<LeftMouse>"] = "MouseClick",
+          ["<2-LeftMouse>"] = "NOP",
         },
         -- Setting any of these to `false` will disable the mapping.
+        popup = {
+          ["?"] = "HelpPopup",
+          ["A"] = "CherryPickPopup",
+          ["d"] = "DiffPopup",
+          ["M"] = "RemotePopup",
+          ["P"] = "PushPopup",
+          ["X"] = "ResetPopup",
+          ["Z"] = "StashPopup",
+          ["i"] = "IgnorePopup",
+          ["t"] = "TagPopup",
+          ["b"] = "BranchPopup",
+          ["B"] = "BisectPopup",
+          ["w"] = "WorktreePopup",
+          ["c"] = "CommitPopup",
+          ["f"] = "FetchPopup",
+          ["l"] = "LogPopup",
+          ["m"] = "MergePopup",
+          ["p"] = "PullPopup",
+          ["r"] = "RebasePopup",
+          ["v"] = "RevertPopup",
+        },
         status = {
+          ["j"] = "MoveDown",
+          ["k"] = "MoveUp",
+          ["o"] = "OpenTree",
           ["q"] = "Close",
           ["I"] = "InitRepo",
           ["1"] = "Depth1",
           ["2"] = "Depth2",
           ["3"] = "Depth3",
           ["4"] = "Depth4",
+          ["Q"] = "Command",
           ["<tab>"] = "Toggle",
           ["x"] = "Discard",
           ["s"] = "Stage",
           ["S"] = "StageUnstaged",
           ["<c-s>"] = "StageAll",
           ["u"] = "Unstage",
+          ["K"] = "Untrack",
           ["U"] = "UnstageStaged",
-          ["d"] = "DiffAtFile",
+          ["y"] = "ShowRefs",
           ["$"] = "CommandHistory",
-          ["#"] = "Console",
+          ["Y"] = "YankSelected",
           ["<c-r>"] = "RefreshBuffer",
-          ["<enter>"] = "GoToFile",
+          ["<cr>"] = "GoToFile",
+          ["<s-cr>"] = "PeekFile",
           ["<c-v>"] = "VSplitOpen",
           ["<c-x>"] = "SplitOpen",
           ["<c-t>"] = "TabOpen",
-          ["?"] = "HelpPopup",
-          ["D"] = "DiffPopup",
-          ["p"] = "PullPopup",
-          ["r"] = "RebasePopup",
-          ["m"] = "MergePopup",
-          ["P"] = "PushPopup",
-          ["c"] = "CommitPopup",
-          ["l"] = false,
-          ["L"] = "LogPopup",
-          ["v"] = "RevertPopup",
-          ["Z"] = "StashPopup",
-          ["A"] = "CherryPickPopup",
-          ["b"] = "BranchPopup",
-          ["f"] = "FetchPopup",
-          ["X"] = "ResetPopup",
-          ["M"] = "RemotePopup",
           ["{"] = "GoToPreviousHunkHeader",
           ["}"] = "GoToNextHunkHeader",
+          ["[c"] = "OpenOrScrollUp",
+          ["]c"] = "OpenOrScrollDown",
+          ["<c-k>"] = "PeekUp",
+          ["<c-j>"] = "PeekDown",
+          ["<c-n>"] = "NextSection",
+          ["<c-p>"] = "PreviousSection",
         },
       },
-    })
+    }
     -- vim.cmd([[
     --   augroup DefaultRefreshEvents
     --     au!
