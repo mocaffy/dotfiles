d9f9eb00f9d43f565c38cb1d79e0468cc90007ba
mocaffy <mocaffy@gmail.com>
2025-05-05 21:45:50 +0900

    astro update

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -25,6 +25,7 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
+        fileformat = "unix",
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
