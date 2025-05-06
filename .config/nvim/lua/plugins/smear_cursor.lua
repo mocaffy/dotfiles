ddc6fd0e6c658e66614b38a83ccd97fbe5402572
mocaffy <mocaffy@gmail.com>
2025-05-06 17:01:16 +0900

    smear_cursor

---
+++ .config/nvim/lua/plugins/smear_cursor.lua
@@ -0,0 +1,12 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    cursor_color = "#ff00ff",
+    -- stiffness = 0.3,
+    -- trailing_stiffness = 0.1,
+    -- trailing_exponent = 5,
+    -- never_draw_over_target = true,
+    -- hide_target_hack = true,
+    -- gamma = 1,
+  },
+}
