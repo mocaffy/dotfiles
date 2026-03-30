be13a7d038311d943e806d0be43604e05a281e3b
mocaffy <mocaffy@gmail.com>
2026-03-31 01:14:25 +0900

    🎨 Karabiner GUI 設定を逆輸入し全デバイス共通の Caps Lock → Ctrl を追加

--- programs/karabiner/default.nix
+++ programs/karabiner/default.nix
@@ -1,7 +1,9 @@
 { config, pkgs, lib, ... }: {
   xdg.configFile = lib.mkIf pkgs.stdenv.isDarwin {
-    "karabiner/karabiner.json".source =
-      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/programs/karabiner/karabiner.json";
+    "karabiner/karabiner.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/programs/karabiner/karabiner.json";
+      force = true;
+    };
     "karabiner/assets".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/programs/karabiner/assets";
   };
