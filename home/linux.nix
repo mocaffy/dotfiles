5d8a052fe8959e651c0e468d7592bf84ad60f7ea
mocaffy <mocaffy@gmail.com>
2026-03-31 01:33:00 +0900

    🎨 tpm プラグインの自動インストールを common.nix に共通化

--- home/linux.nix
+++ home/linux.nix
@@ -24,16 +24,4 @@
     };
   };
 
-  home.activation.tpmInstall = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    _cloneIfMissing() {
-      local repo="$1" dest="$2"
-      if [ ! -d "$dest" ]; then
-        ${pkgs.git}/bin/git clone "https://github.com/$repo" "$dest"
-      fi
-    }
-    _cloneIfMissing tmux-plugins/tpm              "$HOME/.local/share/tmux/plugins/tpm"
-    _cloneIfMissing tmux-plugins/tmux-copycat     "$HOME/.local/share/tmux/plugins/tmux-copycat"
-    _cloneIfMissing tmux-plugins/tmux-open        "$HOME/.local/share/tmux/plugins/tmux-open"
-    _cloneIfMissing mocaffy/tmux-tabicon          "$HOME/.local/share/tmux/plugins/tmux-tabicon"
-  '';
 }
